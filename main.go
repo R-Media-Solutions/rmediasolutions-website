7ae9db4dfe4e9b93a48b1d22ce9af5b14b9247dd
Raden Rifal Ardiansyah <radenrifalardiansyah@gmail.com>
2022-12-10 17:30:58 +0700

    Updated

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 
 func main() {
 	http.HandleFunc("/", authcontroller.Index)
+	http.HandleFunc("/login", authcontroller.Login)
+	http.HandleFunc("/logout", authcontroller.Logout)
+	http.HandleFunc("/register", authcontroller.Register)
 
 	fmt.Println("Server jalan di: http://localhost:3000")
 	http.ListenAndServe(":3000", nil)
