32951a077a2486a2575bb3ead198dce507b58114
Raden Rifal Ardiansyah <radenrifalardiansyah@gmail.com>
2022-12-10 12:03:55 +0700

    Push

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	authcontroller "github.com/R-Media-Solutions/rmediasolutions-website/controllers"
 )
 
 func main() {
+	http.HandleFunc("/", authcontroller.Index)
+
 	fmt.Println("Server jalan di: http://localhost:3000")
 	http.ListenAndServe(":3000", nil)
 }
